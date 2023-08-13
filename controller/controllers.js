7c23bbd9bab41eb79c297eb3352f86a69c47a207
Satyam Gupta <44284062+satya1588@users.noreply.github.com>
2023-08-13 18:35:20 +0530

    Update controllers.js

--- controller/controllers.js
+++ controller/controllers.js
@@ -15,7 +15,7 @@ export const addUser = async (request, response) => {
     const newUser = new User(user);
     try{
         await newUser.save();
-        response.status(201).json(newUser);
+        response.status(200).json(newUser);
     } catch (error){
         response.status(409).json({ message: error.message});     
     }
